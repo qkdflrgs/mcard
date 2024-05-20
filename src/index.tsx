803ff77f2a1e6cff43981c49720455f47bd1e236
qkdflrgs <qkdflrgs@gmail.com>
2024-05-20 14:42:56 +0900

    feat(common): apply globalStyles(emotion) to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import { Global } from '@emotion/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import globalStyles from '@styles/globalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
 )
