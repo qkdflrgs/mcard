1003ea1f7a5f8f2076c4da1eda15f579990ea375
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-05-22 21:04:53 +0900

    feat(cardList): add Home page with card list (#5)
    * feat(cardList): install react-infinite-scroll-component
    
    * feat(cardList): remove unused images at manifest
    
    * feat(cardList): change grey color
    
    * feat(cardList): refactoring getCards for infinite scroll
    
    * feat(cardList): add CardList to App
    
    * feat(cardList): add CardList component
    
    * feat(cardList): add AdBanners component
    
    * feat(cardList): add ListRow component
    
    * feat(cardList): add QueryClient to App
    
    * feat(cardList): add paths

---
+++ src/components/home/AdBanners.tsx
@@ -0,0 +1,45 @@
+import { getAdBanners } from '@/remote/adBanner'
+import { colors } from '@/styles/colorPalette'
+import Flex from '@common/Flex'
+import Text from '@common/Text'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import { useQuery } from 'react-query'
+import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+function AdBanners() {
+  const { data } = useQuery({ queryKey: ['adBanners'], queryFn: getAdBanners })
+
+  return (
+    <Container>
+      <Swiper spaceBetween={8}>
+        {data?.map((banner) => {
+          return (
+            <SwiperSlide key={banner.id}>
+              <Link to={banner.link}>
+                <Flex direction="column" css={bannerContainerStyles}>
+                  <Text bold={true}>{banner.title}</Text>
+                  <Text typography="t7">{banner.description}</Text>
+                </Flex>
+              </Link>
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  padding: 24px;
+`
+
+const bannerContainerStyles = css`
+  padding: 16px;
+  background-color: ${colors.grey};
+  border-radius: 4px;
+`
+
+export default AdBanners
--- src/remote/card.ts
+++ src/remote/card.ts
@@ -1,13 +1,33 @@
 import { COLLECTIONS } from '@constants'
 import { Card } from '@models/card'
-import { collection, getDocs } from 'firebase/firestore'
+import {
+  collection,
+  getDocs,
+  limit,
+  query,
+  QuerySnapshot,
+  startAfter,
+} from 'firebase/firestore'
 import { store } from './firebase'
 
-export async function getCards() {
-  const cardSnapshot = await getDocs(collection(store, COLLECTIONS.CARD))
+export async function getCards(pageParam?: QuerySnapshot<Card>) {
+  const cardQuery =
+    pageParam == null
+      ? query(collection(store, COLLECTIONS.CARD), limit(10))
+      : query(
+          collection(store, COLLECTIONS.CARD),
+          startAfter(pageParam),
+          limit(10),
+        )
 
-  return cardSnapshot.docs.map((doc) => ({
+  const cardSnapshot = await getDocs(cardQuery)
+
+  const lastVisible = cardSnapshot.docs[cardSnapshot.docs.length - 1]
+
+  const items = cardSnapshot.docs.map((doc) => ({
     id: doc.id,
     ...(doc.data() as Card),
   }))
+
+  return { items, lastVisible }
 }
