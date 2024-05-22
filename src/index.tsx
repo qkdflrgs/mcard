985f6c9a10f13d8f5c6045c21520d73d47768e5d
qkdflrgs <qkdflrgs@gmail.com>
2024-05-22 14:40:44 +0900

    feat(common): add Alert context provider to App

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import globalStyles from '@styles/globalStyles'
+import { AlertContextProvider } from '@contexts/AlertContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Global styles={globalStyles} />
-    <App />
+    <AlertContextProvider>
+      <App />
+    </AlertContextProvider>
   </React.StrictMode>,
 )
 
