14e1c42649953833005ddf823b6cdd009454d270
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-05-22 14:43:17 +0900

    feat(common): add Common components (#3)
    * feat(common): add styles path to tsconfig
    
    * feat(common): apply globalStyles(emotion) to App
    
    * feat(common): add color styles
    
    * feat(common): add global styles(with reset styles)
    
    * feat(common): add typo styles
    
    * feat(common): add Button component with styles
    
    * feat(common): add Alert component with styles
    
    * feat(common): add Dimmed component with styles
    
    * feat(common): add Flex component
    
    * feat(common): add Input component with styles
    
    * feat(common): add Text component with styles
    
    * feat(common): add TextField component
    
    * feat(common): add Alert context
    
    * feat(common): add Alert context provider to App
    
    * feat(common): add root-protal div to html
    
    * feat(common): add paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
+import { Global } from '@emotion/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import globalStyles from '@styles/globalStyles'
+import { AlertContextProvider } from '@contexts/AlertContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Global styles={globalStyles} />
+    <AlertContextProvider>
+      <App />
+    </AlertContextProvider>
   </React.StrictMode>,
 )
 
