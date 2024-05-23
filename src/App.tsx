2ade58bade3697f133ddcef0818d46c452f6472c
qkdflrgs <qkdflrgs@gmail.com>
2024-05-23 15:38:03 +0900

    feat(signup): add Signup page to App router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '@pages/Home'
 import TestPage from '@pages/Test'
 import CardPage from '@pages/Card'
-import ScrollToTop from '@components/common/ScrollToTop'
+import ScrollToTop from '@common/ScrollToTop'
+import SigninPage from '@pages/Signin'
+import SignupPage from '@pages/Signup'
+import Navbar from '@common/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
+      <Navbar />
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/test" Component={TestPage} />
         <Route path="/card/:id" Component={CardPage} />
+        <Route path="/signin" Component={SigninPage} />
+        <Route path="/signup" Component={SignupPage} />
       </Routes>
     </BrowserRouter>
   )
