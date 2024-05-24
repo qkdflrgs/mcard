7e90597535a86083cfb1a4e1e1cbc4669b8d063b
qkdflrgs <qkdflrgs@gmail.com>
2024-05-24 18:14:16 +0900

    feat(apply): add ButtonGroup to Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -7,6 +7,9 @@ import {
   buttonSizeMap,
   buttonWeakMap,
 } from '@styles/button'
+import Flex from './Flex'
+import Spacing from './Spacing'
+import Text from './Text'
 
 interface ButtonProps {
   color?: ButtonColor
@@ -16,7 +19,12 @@ interface ButtonProps {
   disabled?: boolean
 }
 
-const Button = styled.button<ButtonProps>(
+interface ButtonGroupProps {
+  title?: string
+  children: React.ReactNode
+}
+
+const BaseButton = styled.button<ButtonProps>(
   {
     cursor: 'pointer',
     fontWeight: 'bold',
@@ -41,5 +49,36 @@ const Button = styled.button<ButtonProps>(
         `
       : undefined,
 )
+
+function ButtonGroup({ title, children }: ButtonGroupProps) {
+  return (
+    <Flex direction="column">
+      {title && (
+        <>
+          <Text typography="t6" bold={true}>
+            {title}
+          </Text>
+          <Spacing size={8} />
+        </>
+      )}
+      <Flex css={buttonGroupStyles}>{children}</Flex>
+    </Flex>
+  )
+}
+
+const buttonGroupStyles = css`
+  flex-wrap: wrap;
+  gap: 10px;
+
+  & button {
+    flex: 1;
+  }
+`
+
+const Button = BaseButton as typeof BaseButton & {
+  Group: typeof ButtonGroup
+}
+
+Button.Group = ButtonGroup
 
 export default Button
