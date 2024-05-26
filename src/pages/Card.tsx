b2cddd7d2432fabc98b1ebf934a9f4416821af4e
qkdflrgs <qkdflrgs@gmail.com>
2024-05-27 03:16:09 +0900

    refactor(UIUX): refactoring removeHtmlTags function

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -102,22 +102,7 @@ function IconCheck() {
 }
 
 function removeHtmlTags(text: string) {
-  let output = ''
-
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === '<') {
-      for (let j = i + 1; j < text.length; j++) {
-        if (text[j] === '>') {
-          i = j
-          break
-        }
-      }
-    } else {
-      output += text[i]
-    }
-  }
-
-  return output
+  return text.replace(/<\/?[^>]+(>|$)/g, '')
 }
 
 const termsContainerStyles = css`
